bd34e15c5ad70d128f12777a63ee6c22e4b775bc
BrettAiken <bma26@njit.edu>
2017-01-21 01:15:38 -0500

    Initial COmmit

---
+++ Assets/ApplyHit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyHit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    float hitPoints = 1.0f;
+ 
+    void ApplyDamage(float damage)
+    {
+        if (hitPoints <= 0.0)
+            return;
+ 
+        hitPoints -= damage;
+
+        if(hitPoints <= 0.0)
+        {
+            Invoke("DelayedDetonate",0);
+        }
+     } 
+ 
+     void DelayedDetonate(){
+        Destroy(gameObject);
+     }
+}
---
+++ Assets/Fire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    float damage;
+    public int distance = 20;
+
+	// Use this for initialization
+	void Start () 
+    {
+        damage = 1.0f;
+	}
+	
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Clicking");
+            FireOneShot();
+        }
+    }
+ 
+    void FireOneShot()
+    {
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        Vector3 localOffset = transform.position + transform.up;
+
+        if (Physics.Raycast(localOffset, direction, out hit, distance))
+        {
+            Debug.DrawLine(localOffset, hit.point, Color.cyan, 2, true);
+            Debug.Log("Hit " + hit.collider.gameObject.name);
+
+            hit.collider.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+        }
+        else
+        {
+            Debug.Log("Missed");
+        }
+    }
+}
